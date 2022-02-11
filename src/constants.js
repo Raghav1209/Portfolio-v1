0bf4863bdff1f0ba8bb5694d27aaf7f8871968fe
Raghav1209 <raghavghai170@gmail.com>
2022-02-11 22:16:43 +0530

    aligned items stretch

--- src/constants.js
+++ src/constants.js
@@ -1,17 +1,17 @@
 const Projects = [
     {
     title: 'Anonymous Complaint Box',
-    description: "This is a Full Realtime Chat Application. It allows you to built scalable and custom chat applications extremely easily. We will use React for the front end, and NodeJS on the backend. It contains stream-chat-react based inbuilt components.",
+    description: "A platform where employers, workers and users can give their anonymous feedbacks or can submit their complaints without having a risk of getting identified. It is a fully authenticated website which includes signUp using GOOGLE, TWITTER, GITHUB along with the conventional method.",
     image: '/images/Secrets.png',
     tags: ['Mongo', 'Express', 'HTML', 'CSS', 'Node'],
-    source: "https://github.com/Raghav1209/Secrets_Authentication",
+    source: "https://github.com/Raghav1209/Anonymous-Complaint-Box",
     // visit: 'https://virtual-classroom-project.netlify.app/',
     id: 0,
   },
   {
     title: 'Blog Website ',
-    description: "This is a Full Realtime Chat Application. It allows you to built scalable and custom chat applications extremely easily. We will use React for the front end, and NodeJS on the backend. It contains stream-chat-react based inbuilt components.",
-      image: '/images/Blog.png',
+    description: "A fully Responsive website where one can compose Blogs and post them on daily basis. These blogs are visible to everyone on the Home page and are stored in the database. It is build using ejs pages.",
+    image: '/images/Blog.png',
     tags: ['Mongo', 'Express', 'HTML', 'CSS', 'Node'],
     source: 'https://github.com/Raghav1209/Blog_website_with_database',
     // visit: 'https://virtual-classroom-project.netlify.app/',
