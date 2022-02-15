bb12ff338a76ea4b8ebf65e80869d5bb38214567
Raghav1209 <raghavghai170@gmail.com>
2022-02-15 20:58:29 +0530

    updated image

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ const Projects = [
     {
     title: 'Anonymous Complaint Box',
     description: "A platform where employers, workers and users can give their anonymous feedbacks or can submit their complaints without having a risk of getting identified. It is a fully authenticated website which includes signUp using GOOGLE, TWITTER, GITHUB along with the conventional method.",
-    image: '/images/Secrets.png',
+    image: '/images/complaints.png',
     tags: ['Mongo', 'Express', 'HTML', 'CSS', 'Node'],
     source: "https://github.com/Raghav1209/Anonymous-Complaint-Box",
     // visit: 'https://virtual-classroom-project.netlify.app/',
