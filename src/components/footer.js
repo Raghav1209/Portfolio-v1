f8d338527dcfad81dcb64f7e48288887ba7f4d4d
Raghav1209 <raghavghai170@gmail.com>
2022-11-14 15:58:50 +0530

    version updated

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,12 +4,12 @@ import { Container } from "react-bootstrap"
 const footer = () => {
 
     const date = new Date();
-    const year = date.getFullYear();
+    // const year = date.getFullYear();
 
     return (
         <footer id="footer">
             <Container className="footerContainer">
-                All rights reserved <i class="fa fa-copyright"></i> {year} Raghav Ghai
+                All rights reserved <i class="fa fa-copyright"></i>Raghav Ghai 2021 v1.1.
             </Container>
         </footer>
     );
